f91625394a06487ebc7923f4f09b80941f0fa7fa
rakibhow203 <rakibhossainhow65@gmail.com>
2024-06-11 21:15:36 +0600

    added use axioscecure

--- index.js
+++ index.js
@@ -47,9 +47,46 @@ async function run() {
    const organizerCollection = client.db("MedicalCamp").collection("Organizers");
 
     
+    
+    
+
+
+     // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
+// middlewares 
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+ // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
     // users related api
-
-
  app.get('/users', async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
